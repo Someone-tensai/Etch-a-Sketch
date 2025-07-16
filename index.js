e5725bed04bbc3a3d016f0481bbfd0a6afa5436c
Someone-tensai <paudelsulav5@gmail.com>
2025-07-16 21:15:16 +0545

    16*16 Gridgit

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 container = document.querySelector(".container");
 
-for(i = 0; i < 256; i++)
+// Create 16 divs inside one div
+// And create 16 of those divs..
+for(i = 0; i < 16; i++)
 {
-    div = document.createElement("div");
-    div.textContent = "Something";
-    div.style.backgroundColor = "red";
-    container.appendChild(div);
+    outer_div = document.createElement("div");
+    outer_div.setAttribute("class", "outer")
+    for(j = 0 ; j < 16; j++)
+    {
+        div = document.createElement("div");
+        div.setAttribute("class", "inner");
+        div.textContent = "Div";
+        outer_div.appendChild(div);
+    }
+   container.appendChild(outer_div);
 }
 
